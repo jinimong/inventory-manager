ae85a284fc3b7035c73dc86a7abb0e4917d9ae67
jwkim <phoongdeng@gmail.com>
2021-03-08 00:27:43 +0900

    chore(server): Add cors whitelist

--- server/config/settings.py
+++ server/config/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "graphene_django",
+    "corsheaders",
     "core",
     "products",
     "events",
@@ -46,11 +47,17 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+CORS_ORIGIN_WHITELIST = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
 ]
 
 ROOT_URLCONF = "config.urls"
--- server/config/urls.py
+++ server/config/urls.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.views.decorators.csrf import csrf_exempt
 from django.urls import path
 
 from graphene_django.views import GraphQLView
@@ -7,5 +8,8 @@ from .schema import schema
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("graphql/", GraphQLView.as_view(graphiql=True, schema=schema)),
+    path(
+        "graphql/",
+        csrf_exempt(GraphQLView.as_view(graphiql=True, schema=schema)),
+    ),
 ]
