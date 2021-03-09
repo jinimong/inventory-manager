56ab971a9d1070d572ac858028374cdbd82c0206
jwkim <phoongdeng@gmail.com>
2021-03-10 00:52:52 +0900

    feat(client): Add Event Page with allEvents query

--- client/src/components/Events.tsx
+++ client/src/components/Events.tsx
@@ -1,7 +1,41 @@
 import React from 'react';
+import { gql, useQuery } from '@apollo/client';
+
+type Event = {
+  id: number;
+  eventType: string;
+  createdAt: string;
+};
+
+type AllEvents = {
+  allEvents: Event[];
+};
+
+const EVENTS = gql`
+  query {
+    allEvents {
+      id
+      eventType
+      createdAt
+    }
+  }
+`;
 
 const Events = () => {
-  return <div>Events</div>;
+  const { loading, error, data } = useQuery<AllEvents>(EVENTS);
+  if (loading || error || !data) {
+    return <div>Loading...</div>;
+  }
+  return (
+    <div>
+      <h3>Events</h3>
+      <ul>
+        {data.allEvents.map((event) => (
+          <li key={event.id}>{event.eventType}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Events;
