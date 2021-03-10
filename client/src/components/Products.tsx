237f4334b96f9ab564510a88900a4f867369707e
jwkim <phoongdeng@gmail.com>
2021-03-10 17:43:00 +0900

    feat(client): Add Products Page

--- client/src/components/Products.tsx
+++ client/src/components/Products.tsx
@@ -1,7 +1,55 @@
 import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import { gql, useQuery } from '@apollo/client';
+import { Product } from '../utils/types';
+
+const PRODUCTS = gql`
+  query {
+    allProducts {
+      id
+      name
+      barcode
+      count
+      materials {
+        name
+      }
+      categories {
+        name
+      }
+    }
+  }
+`;
 
 const Products = () => {
-  return <div>Products</div>;
+  const { loading, error, data } = useQuery<{
+    allProducts: Product[];
+  }>(PRODUCTS);
+  const { pathname } = useLocation();
+  if (loading || error || !data) {
+    return <div>Loading...</div>;
+  }
+  return (
+    <div>
+      <h3>Products</h3>
+      <hr />
+      <ul>
+        {data.allProducts.map((product) => (
+          <li key={product.id}>
+            <Link to={`${pathname}/${product.id}`}>
+              <span>{product.name}</span>
+              <span>
+                ({product.materials.map((material) => material.name).join('/')})
+              </span>
+              <span>
+                ({product.categories.map((category) => category.name).join('/')}
+                )
+              </span>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Products;
