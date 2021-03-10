4327731485ec386b5eaa35e66ed1471028a8606f
jwkim <phoongdeng@gmail.com>
2021-03-10 16:20:58 +0900

    feat(client): Add Page's Link to Home

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
-  return <div>Home!</div>;
+  return (
+    <div>
+      <h3>Home!</h3>
+      <hr />
+      <ul>
+        <li>
+          <Link to="/events">Events</Link>
+        </li>
+        <li>
+          <Link to="/products">Products</Link>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Home;
