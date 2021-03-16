05e6abecf17d0eb749a52ffc90d973b4c2b710de
jwkim <phoongdeng@gmail.com>
2021-03-16 23:57:31 +0900

    feat(client): 입점처 input 조건부렌더링

--- client/src/components/CreateEvent.tsx
+++ client/src/components/CreateEvent.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { gql, useMutation } from '@apollo/client';
 import { useForm } from 'react-hook-form';
+import { EventType, EventTypeMap, eventTypesAboutStore } from '../utils/types';
 
 type InventoryChangeInput = {
   productId: number;
@@ -34,9 +35,12 @@ const CreateEvent: React.FC = () => {
   const [createEvent] = useMutation(CREATE_EVENT);
 
   // eslint-disable-next-line @typescript-eslint/unbound-method
-  const { control, register, handleSubmit, reset } = useForm<EventInput>({
-    defaultValues,
-  });
+  const { control, register, watch, handleSubmit, reset } = useForm<EventInput>(
+    {
+      defaultValues,
+    },
+  );
+  const watchEventType = watch('eventType');
   const onSubmit = (eventInput: EventInput) => console.log(eventInput);
   // createEvent({
   //   variables: { eventInput },
@@ -44,8 +48,16 @@ const CreateEvent: React.FC = () => {
   // }).then(() => reset());
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <input placeholder="Type" name="eventType" ref={register} required />
-      <input placeholder="입점처" name="storeId" ref={register} />
+      <select name="eventType" ref={register} required>
+        {Array.from(EventTypeMap).map(([key, value]) => (
+          <option key={key} value={key}>
+            {value}
+          </option>
+        ))}
+      </select>
+      {eventTypesAboutStore.includes(watchEventType as EventType) && (
+        <input placeholder="입점처" name="storeId" ref={register} />
+      )}
       <textarea placeholder="메모" name="description" ref={register} />
       <button type="submit">Submit</button>
     </form>
--- client/src/utils/types/common.ts
+++ client/src/utils/types/common.ts
@@ -3,3 +3,30 @@ export type OptionType = {
   value: string | number;
   __isNew__?: boolean;
 };
+
+export enum EventType {
+  SELL_DIRECT = 'SD',
+  ORDER_PRODUCT = 'OP',
+  SEND_PRODUCT = 'SP',
+  SETTLE_SALE = 'SS',
+  LEAVE_STORE = 'LS',
+  DEFECT_PRODUCT_IN_STORE = 'DS',
+  DEFECT_PRODUCT_IN_HOME = 'DH',
+}
+
+export const EventTypeMap = new Map([
+  [EventType.SELL_DIRECT, '개인판매'],
+  [EventType.ORDER_PRODUCT, '제품발주'],
+  [EventType.SEND_PRODUCT, '입점처 입고'],
+  [EventType.SETTLE_SALE, '판매내역정산'],
+  [EventType.LEAVE_STORE, '입점처 퇴점'],
+  [EventType.DEFECT_PRODUCT_IN_STORE, '불량:입점처'],
+  [EventType.DEFECT_PRODUCT_IN_HOME, '불량:집'],
+]);
+
+export const eventTypesAboutStore = [
+  EventType.SEND_PRODUCT,
+  EventType.SETTLE_SALE,
+  EventType.LEAVE_STORE,
+  EventType.DEFECT_PRODUCT_IN_STORE,
+];
