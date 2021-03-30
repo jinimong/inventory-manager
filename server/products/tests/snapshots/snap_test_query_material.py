ec7e1e09cc08cf89551a146371ab9957320279a9
jwkim <phoongdeng@gmail.com>
2021-03-31 02:57:08 +0900

    test(server): Material Query Test 보완

--- server/products/tests/snapshots/snap_test_query_material.py
+++ server/products/tests/snapshots/snap_test_query_material.py
@@ -34,3 +34,26 @@ snapshots['test_retrieve_query 1'] = {
         }
     }
 }
+
+snapshots['test_retrieve_query_with_related_object 1'] = {
+    'data': {
+        'material': {
+            'id': '1',
+            'name': 'material',
+            'products': [
+                {
+                    'id': '1',
+                    'name': 'product_0'
+                },
+                {
+                    'id': '2',
+                    'name': 'product_1'
+                },
+                {
+                    'id': '3',
+                    'name': 'product_2'
+                }
+            ]
+        }
+    }
+}
--- server/products/tests/test_query_material.py
+++ server/products/tests/test_query_material.py
@@ -27,16 +27,27 @@ def test_retrieve_query(snapshot, client, product_material_factory):
     snapshot.assert_match(response)
 
 
-# def test_retrieve_query_with_related_object(snapshot, client, product_factory):
-#     product = product_factory()
-#     query = """
-#         query ($id: Int!) {
-#             material(id: $id) {
-#                 id
-#                 name
+def test_retrieve_query_with_related_object(
+    snapshot,
+    client,
+    product_factory,
+    product_material_factory,
+):
+    material = product_material_factory(name="material")
+    for n in range(3):
+        product_factory(name=f"product_{n}", materials=[material])
 
-#             }
-#         }
-#     """
-#     response = client.execute(query, variable_values={"id": material.id})
-#     snapshot.assert_match(response)
+    query = """
+        query ($id: Int!) {
+            material(id: $id) {
+                id
+                name
+                products {
+                    id
+                    name
+                }
+            }
+        }
+    """
+    response = client.execute(query, variable_values={"id": material.id})
+    snapshot.assert_match(response)
