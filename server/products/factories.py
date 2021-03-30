fa8258fee0ed9558fd1e168f8d66b526c1ee7188
jwkim <phoongdeng@gmail.com>
2021-03-31 02:48:07 +0900

    feat(server): M2M 필드 factory 추가 및 테스트

--- server/products/factories.py
+++ server/products/factories.py
@@ -2,25 +2,41 @@ import factory
 
 
 class ProductMaterialFactory(factory.django.DjangoModelFactory):
-    name = factory.Faker("slug")
-
     class Meta:
         model = "products.ProductMaterial"
 
-
-class ProductCategoryFactory(factory.django.DjangoModelFactory):
     name = factory.Faker("slug")
 
+
+class ProductCategoryFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = "products.ProductCategory"
 
+    name = factory.Faker("slug")
+
 
 class ProductFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = "products.Product"
+        django_get_or_create = ("name",)
+
     name = factory.Faker("slug")
     count = factory.Faker("random_int", min=1, max=1000)
     price = 2000
     price_with_pees = factory.LazyAttribute(lambda o: o.price + 500)
 
-    class Meta:
-        model = "products.Product"
-        django_get_or_create = ("name",)
+    @factory.post_generation
+    def materials(self, create, extracted):
+        if not create:
+            return
+
+        if extracted:
+            self.materials.add(*extracted)
+
+    @factory.post_generation
+    def categories(self, create, extracted):
+        if not create:
+            return
+
+        if extracted:
+            self.categories.add(*extracted)
--- server/products/tests/test_factory_product.py
+++ server/products/tests/test_factory_product.py
@@ -7,3 +7,21 @@ def test_create(product_factory):
     product = product_factory()
     assert 1 == Product.objects.count()
     assert product.name == Product.objects.last().name
+
+
+def test_create_with_materials(product_factory, product_material_factory):
+    material_1 = product_material_factory()
+    material_2 = product_material_factory()
+    product = product_factory(materials=[material_1, material_2])
+    assert set(product.materials.values_list("id", flat=True)) == set(
+        [material_1.id, material_2.id]
+    )
+
+
+def test_create_with_categories(product_factory, product_category_factory):
+    category_1 = product_category_factory()
+    category_2 = product_category_factory()
+    product = product_factory(categories=[category_1, category_2])
+    assert set(product.categories.values_list("id", flat=True)) == set(
+        [category_1.id, category_2.id]
+    )
