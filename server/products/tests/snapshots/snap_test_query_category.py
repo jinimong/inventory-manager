8443ddfbae3fa6821913fffc28e0c8971973fba1
jwkim <phoongdeng@gmail.com>
2021-03-31 03:03:01 +0900

    test(server): Category Query Test 보완

--- server/products/tests/snapshots/snap_test_query_category.py
+++ server/products/tests/snapshots/snap_test_query_category.py
@@ -7,11 +7,53 @@ from snapshottest import Snapshot
 
 snapshots = Snapshot()
 
-snapshots['test_query 1'] = {
+snapshots['test_list_query 1'] = {
+    'data': {
+        'allCategories': [
+            {
+                'id': '1',
+                'name': 'category_0'
+            },
+            {
+                'id': '2',
+                'name': 'category_1'
+            },
+            {
+                'id': '3',
+                'name': 'category_2'
+            }
+        ]
+    }
+}
+
+snapshots['test_retrieve_query 1'] = {
     'data': {
         'category': {
             'id': '1',
             'name': 'category'
+        }
+    }
+}
+
+snapshots['test_retrieve_query_with_related_object 1'] = {
+    'data': {
+        'category': {
+            'id': '1',
+            'name': 'category',
+            'products': [
+                {
+                    'id': '1',
+                    'name': 'product_0'
+                },
+                {
+                    'id': '2',
+                    'name': 'product_1'
+                },
+                {
+                    'id': '3',
+                    'name': 'product_2'
+                }
+            ]
         }
     }
 }
--- server/products/tests/test_query_category.py
+++ server/products/tests/test_query_category.py
@@ -1,10 +1,48 @@
-def test_query(snapshot, client, product_category_factory):
+def test_list_query(snapshot, client, product_category_factory):
+    for n in range(3):
+        product_category_factory(name=f"category_{n}")
+    query = """
+        query AllCategories {
+            allCategories {
+                id
+                name
+            }
+        }
+    """
+    response = client.execute(query)
+    snapshot.assert_match(response)
+
+
+def test_retrieve_query(snapshot, client, product_category_factory):
     category = product_category_factory(name="category")
     query = """
         query ($id: Int!) {
             category(id: $id) {
                 id
                 name
+            }
+        }
+    """
+    response = client.execute(query, variable_values={"id": category.id})
+    snapshot.assert_match(response)
+
+
+def test_retrieve_query_with_related_object(
+    snapshot, client, product_factory, product_category_factory,
+):
+    category = product_category_factory(name="category")
+    for n in range(3):
+        product_factory(name=f"product_{n}", categories=[category])
+
+    query = """
+        query ($id: Int!) {
+            category(id: $id) {
+                id
+                name
+                products {
+                    id
+                    name
+                }
             }
         }
     """
